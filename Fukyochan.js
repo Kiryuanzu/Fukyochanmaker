e39701bca1c337bae7bbe1b27f88385bbe46a13b
藤池杏佳 <anzu.nagato.1005@gmail.com>
2017-08-29 01:06:05 +0900

    布教ちゃんメーカーver1(画像保存未実装)

---
+++ Fukyochan.js
@@ -0,0 +1,67 @@
+
+<!--
+//haircolor
+function hair01(){
+	document.hairLink.src ="images/haircolor1.png";
+}
+function hair02(){
+	document.hairLink.src ="images/haircolor2.png";
+}
+function hair03(){
+	document.hairLink.src ="images/haircolor3.png";
+}
+function hair04(){
+	document.hairLink.src ="images/haircolor4.png";
+}
+function hair05(){
+	document.hairLink.src ="images/haircolor5.png";
+}
+
+//eyecolor
+
+function eye01(){
+	document.eyeLink.src ="images/eye1.png";
+}
+function eye02(){
+	document.eyeLink.src ="images/eye2.png";
+}
+function eye03(){
+	document.eyeLink.src ="images/eye3.png";
+}
+function eye04(){
+	document.eyeLink.src ="images/eye4.png";
+}
+function eye05(){
+	document.eyeLink.src ="images/eye5.png";
+}
+
+//skincolor
+function skin01(){
+	document.skinLink.src ="images/skin1.png";
+}
+function skin02(){
+	document.skinLink.src ="images/skin2.png";
+}
+function skin03(){
+	document.skinLink.src ="images/skin3.png";
+}
+
+//clothescolor
+function cloth01(){
+	document.clothLink.src="images/cloth1.png"
+}
+
+function cloth02(){
+	document.clothLink.src="images/cloth2.png"
+}
+function cloth03(){
+	document.clothLink.src="images/cloth3.png"
+}
+function cloth04(){
+	document.clothLink.src="images/cloth4.png"
+}
+function cloth05(){
+	document.clothLink.src="images/cloth5.png"
+}
+
+
